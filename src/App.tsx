49947d39e22d769499acf7ea0531c4594cda5d8e
Luisa Negri <luisaanegri@gmail.com>
2023-09-25 14:35:50 +0200

    feat: add QueryClientProvider to setup React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import LineChart from "./components/LineChart";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/reactquery'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>IoT ECG data visualisation</h1>
       </header>
+      <QueryClientProvider client={queryClient}>
+        <LineChart />
+      </QueryClientProvider>
     </div>
   );
 }
