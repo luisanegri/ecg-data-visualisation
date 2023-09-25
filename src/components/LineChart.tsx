963bd38fe82952a2300c26c3b731ad254fd44718
Luisa Negri <luisaanegri@gmail.com>
2023-09-25 14:54:08 +0200

    feat: Initialize ECG LineChart component with react-chartjs-2

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,54 @@
+/* eslintdisable reactrefresh/onlyexportcomponents */
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js';
+import { Line } from 'reactchartjs2';
+import useECGData from '../hooks/useECGData';
+import React from 'react';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const LineChart = () => {
+    const { ecgData, isLoading, isError, error } = useECGData();
+
+    if (isLoading) return <p>Loading...</p>;
+    if (isError) return <p>Error: {error}</p>;
+
+    const chartData = {
+        labels: ecgData?.map(item => item.Time),
+        datasets: [
+            {
+                label: 'ECG Data',
+                data: ecgData?.map(item => item['1']),
+                borderColor: 'lightgreen',
+                borderWidth: 2,
+                pointRadius: 0,
+            }
+        ]
+    };
+
+    return (
+        <Line data={chartData} />
+    )
+}
+
+
+const MemoizedLineChart = React.memo(LineChart);
+
+export default React.memo(MemoizedLineChart);
